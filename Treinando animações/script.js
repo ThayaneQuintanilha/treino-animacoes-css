2f8a3f451a76f0d0216a71fcaf3f6a45bfc39890
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-08-27 09:08:34 -0300

    Enviando arquivos e algumas animações já feitas

---
+++ Treinando animações/script.js
@@ -0,0 +1,22 @@
+// SIMPLES HAMBURGUER ANIMATION
+
+// const hamburguerMenu = document.querySelector('.hamburguer');
+
+// hamburguerMenu.addEventListener('click', () => {
+//   hamburguerMenu.classList.toggle('active');
+// });
+
+// SIMPLE FLOAT
+
+// const button = document.querySelector('.rotate');
+// const quadrado = document.querySelector('.quadrado');
+// const rotateLeave = document.querySelector('.rotateLeave')
+
+// button.addEventListener('click', () => {
+//   quadrado.classList.toggle('rotate');
+// });
+
+// SCROLL REVEL
+
+// ScrollReveal({ reset: true });
+// ScrollReveal().reveal('.testa', { delay: 100 });
